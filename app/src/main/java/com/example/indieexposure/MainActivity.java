ae5d900c93988db65aaf2725fe154a56c5335586
Ivan Santos <201860430@ucc.mx>
2022-05-14 19:00:39 -0500

    Conectado a FireStorage

--- app/src/main/java/com/example/indieexposure/MainActivity.java
+++ app/src/main/java/com/example/indieexposure/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        StorageReference storageRef = storage.getReference();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 }
